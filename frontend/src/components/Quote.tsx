c9f205d555bb6a21ee8ec9cc492eec5981bfa7dd
Srimanthbabugattamaneni <144981709+Srimanthbabugattamaneni@users.noreply.github.com>
2024-09-24 00:59:47 -0400

    applied signup feature

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,9 @@
+
+export const Quote = () =>{
+    return <div className="bg-slate-200 flex justify-center items-center p-10 h-full">
+    <div className="max-w-md text-center text-3xl font-bold">
+      "Hi from quote"
+    </div>
+  </div>
+
+}
---
+++ frontend/src/pages/signup.tsx
@@ -0,0 +1,15 @@
+import { Quote } from "../components/Quote"
+
+export const Signup =()=>{
+    return <div>
+        <div className="grid grid-cols-2">
+        <div className="flex justify-center items-center">
+                
+            </div>
+            <Quote />
+
+        </div>
+        
+    </div>
+    
+} 
