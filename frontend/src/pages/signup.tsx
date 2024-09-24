0a4859b540106d2bcec6efd61c00cd4da8a52d09
Srimanthbabugattamaneni <144981709+Srimanthbabugattamaneni@users.noreply.github.com>
2024-09-24 15:54:22 -0400

    introduced the signin/signup componet

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,12 +1,19 @@
+import { Auth } from "../components/Auth"
 import { Quote } from "../components/Quote"
 
 export const Signup =()=>{
     return <div>
-        <div className="grid grid-cols-2">
-        <div className="flex justify-center items-center">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+                <Auth type = "signup" />
+       
                 
             </div>
-            <Quote />
+            <div className="hidden lg:block">
+              <Quote />
+
+            </div>
+            
 
         </div>
         
